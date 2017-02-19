ad75ea1a08426cf9cedb7da4c5d6be8d193441e8
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-18 18:44:52 -0700

    Test user saves to database

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe User, type: :model do
   it {is_expected.to have_db_column :username }
   it {is_expected.to have_db_column :uuid }
   it {is_expected.to have_db_column :token }
+
+  context 'when valid' do
+    it 'successfully saves to the database' do
+      user = create(:user)
+      saved_user = User.first
+
+      expect(User.count).to eq 1
+      expect(saved_user.first_name).to eq user.first_name
+    end
+  end
 end
