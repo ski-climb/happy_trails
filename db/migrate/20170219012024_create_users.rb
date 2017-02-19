5a8d5102bb0ec015e545175e5ebe225240e44bc3
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-18 18:21:39 -0700

    Create user model.

---
+++ db/migrate/20170219012024_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.integer :uuid
+      t.string :token
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    uuid 1
+    token "MyString"
+    first_name "MyString"
+    last_name "MyString"
+    username "MyString"
+  end
+end
