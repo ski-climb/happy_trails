a6a1cdd419fa462ee366edd5c2d3e8dfd01cceaa
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-19 12:12:03 -0700

    Add failing user login with strava spec

---
+++ spec/features/users/login_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "Successful login" do
+
+  context "given the user does not exist but has a Strava account" do
+    scenario "a new user signs in", vcr: true do
+      expect(User.count).to eq 0
+
+      login_with_strava
+
+      expect(current_path).to eq issues_path
+      expect(page).to have_content "Logged in as"
+      expect(User.count).to eq 1
+    end
+  end
+
+  context "given the user exists and has a Strava account" do
+    scenario "an existing user signs in", vcr: true do
+      user = create(:user, :strava)
+      expect(User.count).to eq 1
+
+      login_with_strava
+
+      expect(current_path).to eq issues_path
+      expect(page).to have_content "Logged in as"
+      expect(User.count).to eq 1
+    end
+  end
+
+  def login_with_strava
+    Capybara.current_driver = :mechanize
+    visit root_path
+    click_on "Log in with Strava"
+    fill_in "email", with: ENV["STRAVA_EMAIL"]
+    fill_in "password", with: ENV["STRAVA_PASSWORD"]
+    click_button "Log In"
+  end
+end
