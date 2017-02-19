df355f77e09a959ca48361832877d2ceaef863c8
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-18 18:35:47 -0700

    Test user database columns.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {is_expected.to have_db_column :first_name }
+  it {is_expected.to have_db_column :last_name }
+  it {is_expected.to have_db_column :username }
+  it {is_expected.to have_db_column :uuid }
+  it {is_expected.to have_db_column :token }
 end
