46b92af41471cb68a4384fdf2fcefbb43c19aede
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-20 15:19:34 -0700

    Seed all resources except photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require './db/seeds/issues_seed'
+
+class Seed
+  def self.start
+    seed = Seed.new
+    seed.remove_all_existing_data
+    seed.generate_admin
+    seed.generate_users
+    seed.generate_issues
+    seed.generate_comments
+    seed.generate_photos
+  end
+
+   def remove_all_existing_data
+    puts "Clearing out the data currently in your database (Ctrl-C now if that's a bad thing...)"
+    sleep(5)
+    puts "Too late!"
+    Admin.destroy_all
+    User.destroy_all
+    Issue.destroy_all
+    Comment.destroy_all
+    Photo.destroy_all
+    puts "Creating fresh, new data"
+  end
+
+  def generate_admin
+    Admin.create!(
+      first_name: 'Trail',
+      last_name:  'God',
+      email:      'admin@example.com',
+      password:   'password'
+    )
+  end
+
+  def generate_users
+    4.times { FactoryGirl.create(:user) }
+  end
+
+  def generate_issues
+    IssuesSeed.start
+  end
+
+  def generate_comments
+    20.times do
+      FactoryGirl.create(:comment, user: User.all.sample, issue: Issue.all.sample)
+    end
+  end
+
+  def generate_photos
+
+  end
+end
+
+Seed.start
+
