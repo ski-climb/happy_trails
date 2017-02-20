ae3e8b464425ee99abfe249bf41bd9b2b2f0d27c
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-20 09:26:46 -0700

    Change find or create from auth hash to use find or create by.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,11 @@ class User < ApplicationRecord
   has_many :photos
 
   def self.find_or_create_from_auth_hash(auth_hash)
-    user = User.find_or_create_by(uuid: auth_hash['uid'])
-    user.update_attributes({
-      token:      auth_hash['credentials']['token'],
-      first_name: auth_hash['info']['first_name'],
-      last_name:  auth_hash['info']['last_name'],
-      username:   auth_hash['info']['name']
-    })
-    user
+    find_or_create_by(uuid: auth_hash['uid']) do |new_user|
+      new_user.token      = auth_hash['credentials']['token']
+      new_user.first_name = auth_hash['info']['first_name']
+      new_user.last_name  = auth_hash['info']['last_name']
+      new_user.username   = auth_hash['info']['name']
+    end
   end
 end
