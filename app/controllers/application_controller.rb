b18d9c1b35e718f229e2b5d01bb8cb43923201a8
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-20 14:07:45 -0700

    Add authorize before action to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
   helper_method :current_user
+  before_action :authorize!
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize!
+    render file: 'public/404', status: 404 unless authorized?
+  end
+
+  def authorized?
+    Permission.new(current_user, params[:controller], params[:action]).authorized?
   end
 end
