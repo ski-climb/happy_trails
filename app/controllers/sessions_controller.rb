7058af04fa04696622eff6338d8dd009239461ef
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-20 12:58:37 -0700

    Use info flash message for better visibility

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = user.id
-    redirect_to root_path, success: "Welcome #{user.first_name}!"
+    redirect_to root_path, info: "Welcome #{user.first_name}!"
   end
 
   def destroy
