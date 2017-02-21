886173ea23f9ad41c8daa28ee281b4d13b3d237a
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-20 19:11:05 -0700

    Add issues and photos seed file

--- db/seeds/issues_seed.rb
+++ db/seeds/issues_seed.rb
@@ -5,9 +5,9 @@ class IssuesSeed
   end
 
   def generate
-    Issue.create!(
+    issue_1 = Issue.create!(
       user:         User.all.sample,
-      title:        'Down Tree on Mesa Trail',
+      title:        'Downed Tree on Mesa Trail',
       description:  'Covering the entire trails. About 12-inch diameter.',
       category:     'obstacle',
       severity:     'low',
@@ -15,7 +15,11 @@ class IssuesSeed
       longitude:    -105.284937
     )
 
-    Issue.create!(
+    issue_1.photos.create!( user: issue_1.user,
+                            url:  Rails.root.join('spec/fixtures/downed_tree.jpg').open
+                          )
+
+    issue_2 = Issue.create!(
       user:         User.all.sample,
       title:        'Standing water near First Flatiron',
       description:  '10-foot stretch of 3-inch deep water. It has not rained recently.',
@@ -25,7 +29,11 @@ class IssuesSeed
       longitude:    -105.291461
     )
 
-    Issue.create!(
+    issue_2.photos.create!( user: issue_2.user,
+                            url:  Rails.root.join('spec/fixtures/flooded_trail.jpg').open
+                          )
+
+    issue_3 = Issue.create!(
       user:         User.all.sample,
       title:        'Peregrine nest near Royal Arch',
       description:  'Nest is near enough to the trail it should be closed immediately.',
@@ -35,10 +43,14 @@ class IssuesSeed
       longitude:    -105.291288
     )
 
-    Issue.create!(
+    issue_3.photos.create!( user: issue_3.user,
+                            url:  Rails.root.join('spec/fixtures/peregrine.jpg').open
+                          )
+
+    issue_4 = Issue.create!(
       user:         User.all.sample,
       title:        'Washout on Kohler Mesa Trail',
-      description:  'Half the trail wiped out for 20 yard stretch.',
+      description:  'Bridge wiped out on trail wiped.',
       category:     'washout',
       severity:     'medium',
       latitude:     39.988893,
@@ -46,18 +58,26 @@ class IssuesSeed
       resolved:     true
     )
 
-    Issue.create!(
+    issue_4.photos.create!( user: issue_4.user,
+                            url:  Rails.root.join('spec/fixtures/bridge_damaged_on_trail.jpg').open
+                          )
+
+    issue_5 = Issue.create!(
       user:         User.all.sample,
-      title:        'Litter at Enchanted Mesa Trail Head',
-      description:  'Trash and toilet paper abound.',
-      category:     'other',
+      title:        'Downed Tree at Enchanted Mesa Trail Head',
+      description:  'Difficult to step over.',
+      category:     'obstacle',
       severity:     'low',
       latitude:     39.992328,
       longitude:    -105.281564,
       resolved:     true
     )
 
-    Issue.create!(
+    issue_5.photos.create!( user: issue_5.user,
+                            url:  Rails.root.join('spec/fixtures/downed_tree.jpg').open
+                          )
+
+    issue_6 = Issue.create!(
       user:         User.all.sample,
       title:        'Mud on Baseline Trail',
       description:  'Barely possible to walk without getting stuck.',
@@ -67,7 +87,11 @@ class IssuesSeed
       longitude:    -105.287902
     )
 
-    Issue.create!(
+    issue_6.photos.create!( user: issue_6.user,
+                            url:  Rails.root.join('spec/fixtures/flooded_trail.jpg').open
+                          )
+
+    issue_7 = Issue.create!(
       user:         User.all.sample,
       title:        'Downed Tree along Shanahan',
       description:  '16-inch diameter.',
@@ -77,9 +101,13 @@ class IssuesSeed
       longitude:    -105.277050,
     )
 
-    Issue.create!(
+    issue_7.photos.create!( user: issue_7.user,
+                            url:  Rails.root.join('spec/fixtures/downed_tree.jpg').open
+                          )
+
+    issue_8 = Issue.create!(
       user:         User.all.sample,
-      title:        'Washout on Fern Canyon Trail',
+      title:        'Bridge Damage on Fern Canyon Trail',
       description:  'Lingering damage from Sept. 2013 flood.',
       category:     'washout',
       severity:     'low',
@@ -87,24 +115,36 @@ class IssuesSeed
       longitude:    -105.291519,
     )
 
-    Issue.create!(
+    issue_8.photos.create!( user: issue_8.user,
+                            url:  Rails.root.join('spec/fixtures/bridge_damaged_on_trail.jpg').open
+                          )
+
+    issue_9 = Issue.create!(
       user:         User.all.sample,
-      title:        'Landslide near Devils Thumb',
+      title:        'Destroyed bridge near Devils Thumb',
       description:  'Along trail in Shadow Canyon.',
       category:     'landslide',
-      severity:     'low',
+      severity:     'high',
       latitude:     39.951805,
       longitude:    -105.293056,
     )
 
-    Issue.create!(
+    issue_9.photos.create!( user: issue_9.user,
+                            url:  Rails.root.join('spec/fixtures/bridge_damaged_on_trail.jpg').open
+                          )
+
+    issue_10 = Issue.create!(
       user:         User.all.sample,
-      title:        'Copious mud along Homestead Trail',
-      description:  'Entire quarter mile stretch of trail.',
-      category:     'mud',
+      title:        'Large rock along Homestead Trail',
+      description:  'Enormous enormous rock.',
+      category:     'obstacle',
       severity:     'medium',
       latitude:     39.939460,
       longitude:    -105.266214,
     )
+
+    issue_10.photos.create!( user: issue_10.user,
+                             url:  Rails.root.join('spec/fixtures/boulder_on_trail.jpg').open
+                           )
   end
 end
