f4a00c6d03324ea3ccfc69dadb8373247bfbb2a9
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-21 16:44:06 -0700

    Permit guest and user to access issues API index method

--- app/services/permission.rb
+++ app/services/permission.rb
@@ -18,12 +18,14 @@ class Permission
     attr_reader :user, :controller, :action
 
     def permitted_for_user?
-      return true if controller == 'sessions' && action.in?(%w(destroy))
-      return true if controller == 'issues'   && action.in?(%w(index new create))
+      return true if controller == 'sessions'      && action.in?(%w(destroy))
+      return true if controller == 'issues'        && action.in?(%w(index new create))
+      return true if controller == 'api/v1/issues' && action.in?(%w(index))
     end
 
     def permitted_for_guest?
       return true if controller == 'sessions' && action.in?(%w(new create))
       return true if controller == 'issues' &&   action.in?(%w(index))
+      return true if controller == 'api/v1/issues' && action.in?(%w(index))
     end
 end
