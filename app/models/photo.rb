384ee0e1901a0a6ffde07f9c6e508f5c0578a684
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-20 17:05:46 -0700

    Comments are optional for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :admin, optional: true
-  belongs_to :comment
+  belongs_to :comment, optional: true
   belongs_to :issue
 
   validates :url, presence: true
