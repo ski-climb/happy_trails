490fa63a91dae01b4f79f629af074fa4739b1cf9
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-21 20:32:50 -0700

    Fix indentations in feature helpers

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -20,25 +20,25 @@ def login_with_strava
   click_on "Login with Strava"
 end
 
-  def stub_new_user_omniauth
-    OmniAuth.config.mock_auth[:strava] = OmniAuth::AuthHash.new({
-          uid:          rand(1000),
-          info:         { first_name:     Faker::Name.name,
-                          last_name:      Faker::Name.last_name,
-                          username:       Faker::Internet.user_name,
-                        },
-          credentials:  { token:          Faker::Internet.password }
-        })
-  end
+def stub_new_user_omniauth
+  OmniAuth.config.mock_auth[:strava] = OmniAuth::AuthHash.new({
+        uid:          rand(1000),
+        info:         { first_name:     Faker::Name.name,
+                        last_name:      Faker::Name.last_name,
+                        username:       Faker::Internet.user_name,
+                      },
+        credentials:  { token:          Faker::Internet.password }
+      })
+end
 
-  def stub_existing_user_omniauth(user)
-    OmniAuth.config.mock_auth[:strava] = OmniAuth::AuthHash.new({
-          uid:          user.uuid,
-          info:         { first_name:     user.first_name,
-                          last_name:      user.last_name,
-                          username:       user.username,
-                        },
-          credentials:  { token:          user.token }
-        })
-  end
+def stub_existing_user_omniauth(user)
+  OmniAuth.config.mock_auth[:strava] = OmniAuth::AuthHash.new({
+        uid:          user.uuid,
+        info:         { first_name:     user.first_name,
+                        last_name:      user.last_name,
+                        username:       user.username,
+                      },
+        credentials:  { token:          user.token }
+      })
+end
 
