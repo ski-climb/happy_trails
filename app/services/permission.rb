b313e2d2d4c95ea8bec5de02f80e33296119be7d
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-23 16:49:31 -0700

    Recent routes api permissions
    Only users and admin can access the recent routes endpoint. Guest
    visitors will not have an access token to get any strava routes.

--- app/services/permission.rb
+++ app/services/permission.rb
@@ -25,14 +25,15 @@ class Permission
 
     def permitted_for_user?
       return true if permitted_for_guest?
-      return true if controller == 'sessions' && action.in?(%w(destroy))
-      return true if controller == 'issues'   && action.in?(%w(new create))
+      return true if controller == 'sessions'              && action.in?(%w(destroy))
+      return true if controller == 'issues'                && action.in?(%w(new create))
+      return true if controller == 'api/v1/recent_routes'  && action.in?(%w(index))
     end
 
     def permitted_for_guest?
       return true if controller == 'sessions'       && action.in?(%w(new create))
-      return true if controller == 'issues'         &&   action.in?(%w(index))
-      return true if controller == 'admin/sessions' &&   action.in?(%w(new create))
-      return true if controller == 'api/v1/issues' && action.in?(%w(index))
+      return true if controller == 'issues'         && action.in?(%w(index))
+      return true if controller == 'admin/sessions' && action.in?(%w(new create))
+      return true if controller == 'api/v1/issues'  && action.in?(%w(index))
     end
 end
