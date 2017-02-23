7c47f9154c36e356c26121dcbb8ab5914d2e4b4c
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-23 16:50:07 -0700

    Add login user feature helper

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,3 +1,7 @@
+def login_user(user)
+  allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+end
+
 def logged_in_user
   user = create(:user)
   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
