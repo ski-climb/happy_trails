6819c03ee603d0ebf0a122d64a6d83e8e107ebda
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-23 17:10:49 -0700

    Add routes service
    Responsible for hitting the Strava API and parsing the response to just
    return the summary polylines.

---
+++ app/services/routes_service.rb
@@ -0,0 +1,26 @@
+class RoutesService
+
+  def self.recent_routes(token)
+    new(token).recent_routes
+  end
+
+  attr_reader :token, :conn
+
+  def initialize(token)
+    @token = token
+    @conn = Faraday.new(url: "https://www.strava.com/api/v3/athlete/activities") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def recent_routes
+    response = conn.get { |req| req.params['access_token'] = token }
+    raw_routes = JSON.parse(response.body)[0..2]
+    summary_polylines(raw_routes)
+  end
+
+  def summary_polylines(raw_routes)
+    raw_routes.map { |raw_route| raw_route['map']['summary_polyline'] }
+  end
+
+end
