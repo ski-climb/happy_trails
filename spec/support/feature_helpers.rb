5b5b11a2c6630f78f8ed970edf18e053cff6d13b
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-24 10:08:05 -0700

    Move stub user helper method into request helpers

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,7 +1,3 @@
-def stub_user(user)
-  allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-end
-
 def logged_in_user
   user = create(:user)
   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
