ae044d2d038c36563f54e4368c5b74ff81241460
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-23 19:31:36 -0700

    Refactor route service

--- app/services/routes_service.rb
+++ app/services/routes_service.rb
@@ -4,22 +4,25 @@ class RoutesService
     new(token).recent_routes
   end
 
-  attr_reader :token, :conn
-
-  def initialize(token)
-    @token = token
-    @conn = Faraday.new(url: "https://www.strava.com/api/v3/athlete/activities") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-  end
-
   def recent_routes
     response = conn.get { |req| req.params['access_token'] = token }
     raw_routes = JSON.parse(response.body)[0..2]
     summary_polylines(raw_routes) rescue []
   end
 
-  def summary_polylines(raw_routes)
-    raw_routes.map { |raw_route| raw_route['map']['summary_polyline'] }
-  end
+  private
+
+    attr_reader :token, :conn
+
+    def initialize(token)
+      @token = token
+      @conn = Faraday.new(url: "https://www.strava.com/api/v3/athlete/activities") do |faraday|
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+
+
+    def summary_polylines(raw_routes)
+      raw_routes.map { |raw_route| raw_route['map']['summary_polyline'] }
+    end
 end
