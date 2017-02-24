1831d9fd67877dd0efd025ee789a7ca72c8ac4e8
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-24 10:39:37 -0700

    Edit recent routes api endpoint route
    Change the route to be nested within the user resource so a user id is
    passed into the request.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :issues, only: [:index]
-      get '/recent_routes', to: 'recent_routes#index'
+      resources :users, only: [] do
+        get '/recent_routes', to: 'recent_routes#index'
+      end
     end
   end
 end
