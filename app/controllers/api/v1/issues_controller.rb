5c2db5557cbb198efb3dd1c1baa13296fb1df976
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-24 17:31:47 -0700

    Edit issues endpoint
    Assign owner to a pin when retrieving the pins using the cookie set
    when a user logs in.

--- app/controllers/api/v1/issues_controller.rb
+++ app/controllers/api/v1/issues_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::IssuesController < ApplicationController
   def index
-    current_id = current_user.id if current_user
+    current_id = cookies[:id] ? cookies[:id].to_i : -1
     render json: Issue.all, current_id: current_id
   end
 end
