724108c516cc8dcfdc3f625cda76f520e2a9bdd5
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-25 18:06:43 -0700

    Display user’s abbreviated name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates_presence_of :username
+
   has_many :issues
   has_many :comments
   has_many :photos
@@ -9,6 +11,14 @@ class User < ApplicationRecord
       new_user.first_name = auth_hash['info']['first_name']
       new_user.last_name  = auth_hash['info']['last_name']
       new_user.username   = auth_hash['info']['name']
+    end
+  end
+
+  def abbreviated_name
+    if first_name && last_name
+      "#{first_name.capitalize} #{last_name.capitalize.first}."
+    else
+      username
     end
   end
 end
