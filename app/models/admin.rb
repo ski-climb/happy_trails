a76d3fcb2f3b5d43564f400f0f86110264f9022b
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-25 18:08:54 -0700

    Validate Admin attributes

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,7 @@
 class Admin < ApplicationRecord
+  validates_presence_of :first_name,
+                        :last_name,
+                        :email
   has_secure_password
   has_many :issues
   has_many :comments
