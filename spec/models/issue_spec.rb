b7e53ae376b05d1f40f89f9c62eeef5d0317023d
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-26 20:31:08 -0700

    Restrict lat/lon values to be physically possible

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -19,6 +19,8 @@ RSpec.describe Issue, type: :model do
     it { is_expected.to validate_presence_of :latitude }
     it { is_expected.to validate_presence_of :longitude }
     it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_numericality_of(:latitude).is_greater_than_or_equal_to(-90).is_less_than_or_equal_to(90) }
+    it { is_expected.to validate_numericality_of(:longitude).is_greater_than_or_equal_to(-180).is_less_than_or_equal_to(180) }
   end
 
   context 'relationships' do
