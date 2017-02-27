ca686c90edeb1bf90514399379a2ed288ae60f68
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-26 20:31:08 -0700

    Restrict lat/lon values to be physically possible

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,7 @@
 class Issue < ApplicationRecord
   validates :description, :category, :severity, :title, :latitude, :longitude, presence: true
+  validates_numericality_of :latitude, greater_than_or_equal_to: -90, less_than_or_equal_to: 90
+  validates_numericality_of :longitude, greater_than_or_equal_to: -180, less_than_or_equal_to: 180
 
   belongs_to :user, optional: true
   belongs_to :admin, optional: true
