a59f179437ce85bcac3d9740f38c0a37470abf57
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-28 16:34:19 -0700

    Add spec for photo with no gps metadata
    Create a spec that tests the redirect to the edit issue path. The
    javascript that ensues is not tested.

--- spec/features/users/create_issue_spec.rb
+++ spec/features/users/create_issue_spec.rb
@@ -18,7 +18,7 @@ describe 'Create issue' do
     end
 
     context 'with a photo with gps metadata' do
-      it 'adds issue to database', vcr: true do
+      it 'adds issue to database' do
         expect(Issue.count).to eq 0
         expect(page).to have_content 'Add an Issue'
 
@@ -43,6 +43,39 @@ describe 'Create issue' do
         expect(issue.severity).to eq 'high'
         expect(issue.latitude).to eq 39.7507388888889
         expect(issue.longitude).to eq -104.996880555556
+        expect(issue.resolved).to be_falsey
+        expect(issue.photos.count).to eq 1
+      end
+    end
+
+    context 'with a photo without gps metadata' do
+      it 'redirects to edit page' do
+        expect(Issue.count).to eq 0
+        expect(page).to have_content 'Add an Issue'
+
+        fill_in 'issue[title]',       with: 'Downed Tree'
+        fill_in 'issue[description]', with: 'It is in the middle of the trail.'
+        select  'Obstacle',           from: 'issue[category]'
+        select  'High',               from: 'issue[severity]'
+
+        attach_file "image[]", Rails.root + "spec/fixtures/peregrine.jpg"
+
+        click_on 'Submit Issue'
+
+        issue = Issue.first
+
+        expect(current_path).to eq edit_issue_path(issue)
+        expect(Issue.count).to eq 1
+
+        expect(page).to have_content 'Could not find GPS data from image. Please select the issue location 
+          by dragging the blue dot to its location and clicking submit location.'
+
+        expect(issue.title).to eq 'Downed Tree'
+        expect(issue.description).to eq 'It is in the middle of the trail.'
+        expect(issue.category).to eq 'obstacle'
+        expect(issue.severity).to eq 'high'
+        expect(issue.latitude).to be_nil
+        expect(issue.longitude).to be_nil
         expect(issue.resolved).to be_falsey
         expect(issue.photos.count).to eq 1
       end
