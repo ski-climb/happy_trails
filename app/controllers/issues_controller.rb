2252b2d8811488f0be665bacb624e03dd660bb11
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-28 16:35:00 -0700

    Extract logic into photo service
    Refactor the issues controller to use a photo service that determines
    the path and flash message based on the gps metadata.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -18,10 +18,9 @@ class IssuesController < ApplicationController
 
   def create
     @issue = current_user.issues.new(issue_params)
-    if set_issue_gps_data && @issue.save
-      save_photo_and_redirect_to_root
-    elsif @issue.save
-      save_photo_and_redirect_to_add_gps_data
+    if @issue.save
+      photo_service = PhotoService.new(image_params, @issue, current_user)
+      redirect_to photo_service.path, info: photo_service.flash
     else
       render :new
     end
@@ -45,29 +44,7 @@ class IssuesController < ApplicationController
       params["image"].first
     end
 
-    def set_issue_gps_data
-      if params["image"] && gps_data = EXIFR::JPEG.new(image_params.open).gps
-        @issue.assign_attributes(latitude: gps_data.latitude, longitude: gps_data.longitude)
-        return true
-      end
-    end
-
     def set_issue
       @issue = Issue.find(params[:id])
-    end
-
-    def add_photo
-      @issue.photos.create(url: image_params, user: current_user)
-    end
-
-    def save_photo_and_redirect_to_root
-      add_photo
-      redirect_to root_path, success: 'Issue added.'
-    end
-
-    def save_photo_and_redirect_to_add_gps_data
-      add_photo
-      flash[:warning] = 'Could not find GPS data from image. Please select the issue location by dragging the blue dot to its location and clicking submit location.'
-      redirect_to edit_issue_path(@issue)
     end
 end
