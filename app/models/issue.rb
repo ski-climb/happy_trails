6895b3e4d885d92e83d2cf5299705659ff12389e
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-27 18:07:45 -0700

    Remove issue validations on lat and lon

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,7 +1,5 @@
 class Issue < ApplicationRecord
-  validates :description, :category, :severity, :title, :latitude, :longitude, presence: true
-  validates_numericality_of :latitude, greater_than_or_equal_to: -90, less_than_or_equal_to: 90
-  validates_numericality_of :longitude, greater_than_or_equal_to: -180, less_than_or_equal_to: 180
+  validates :description, :category, :severity, :title, presence: true
 
   belongs_to :user, optional: true
   belongs_to :admin, optional: true
