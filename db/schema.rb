c1af6856944655b270afc08be54b91942754350d
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-03-01 13:50:12 -0700

    Change photos url to image db migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301025056) do
+ActiveRecord::Schema.define(version: 20170301204052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 20170301025056) do
     t.integer  "comment_id"
     t.integer  "issue_id"
     t.integer  "admin_id"
-    t.string   "url"
+    t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["admin_id"], name: "index_photos_on_admin_id", using: :btree
