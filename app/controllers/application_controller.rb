314d5c20f409cac33cba050ebe981cd073e8a79d
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-03-01 12:09:11 -0700

    Add ‘current_user?’ helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
-  helper_method :current_user, :current_person
+  helper_method :current_user,
+                :current_user?,
+                :current_person
   before_action :authorize!
+
+  def current_user?
+    !! current_user
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
