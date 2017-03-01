cd333e8cc7c759304d3e3b28ef5e69dba1e9c0c9
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-28 20:54:33 -0700

    Put add a trail day link in navbar for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
-  helper_method :current_user, :current_person
+  helper_method :current_user, :current_person, :current_admin
   before_action :authorize!
 
   def current_user
