0b318caa94cd75246ea9e370e2bc699fe2f85c01
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-03-01 11:40:12 -0700

    Edit trail day email addresses to be separated by spaces

--- spec/factories/trail_days.rb
+++ spec/factories/trail_days.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :trail_day do
-    participant_email_addresses { Faker::Internet.email + ', ' + Faker::Internet.email }
+    participant_email_addresses { Faker::Internet.email + ' ' + Faker::Internet.email }
     start_time                  { DateTime.now }
     duration_in_hours           { rand(8) }
     description                 { Faker::Hipster.paragraph }
